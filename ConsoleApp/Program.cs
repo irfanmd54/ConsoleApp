306dd713abdc763ea486936180d2f7195c559f1b
Mohammed, Irfan <Irfan.Mohammed@azdcs.onmicrosoft.com>
2021-09-24 11:57:37 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            int target = 9;
+            int[] output = new TwoSum().TwoSumHashMap(nums, target);
+        }
+    }
+}
---
+++ ConsoleApp/TwoSum/TwoSum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    public class TwoSum
+    {
+        public int[] TwoSumArray(int[] nums, int target)
+        {
+            int[] output = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums.Contains(target - nums[i]) && Array.FindIndex(nums, val => val.Equals(target - nums[i])) != i)
+                {
+                    output[0] = i;
+                    output[1] = Array.FindIndex(nums, val => val.Equals(target - nums[i]));
+
+                    return output;
+                }
+            }
+
+            return output;
+        }
+
+        public int[] TwoSumHashMap(int[] nums, int target)
+        {
+            int[] output = new int[2];
+            Dictionary<int, int> input = new Dictionary<int, int>();
+            int i = 0;
+            foreach (var number in nums)
+            {
+                input.Add(i++, number);
+            }
+
+            nums = new int[nums.Length];
+
+            foreach (var number in input)
+            {
+                if (input.ContainsValue(target - number.Value))
+                {
+                    output[0] = number.Key;
+                    output[1] = input.First(x => x.Value == target - number.Value).Key;
+                }
+            }
+
+            return output;
+        }
+    }
+}
