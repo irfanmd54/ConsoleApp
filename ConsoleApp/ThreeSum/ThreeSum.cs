f365e30b0e86b775a85009218990ca663414a9ca
Mohammed, Irfan <Irfan.Mohammed@azdcs.onmicrosoft.com>
2021-09-24 12:02:29 +0530

    Three Sum template

---
+++ ConsoleApp/ThreeSum/ThreeSum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.ThreeSum
+{
+    public class ThreeSum
+    {
+        public IList<IList<int>> ThreeSumHashMap(int[] nums)
+        {
+            List<List<int>> output = new List<List<int>>();
+
+            return output;
+        }
+    }
+}
